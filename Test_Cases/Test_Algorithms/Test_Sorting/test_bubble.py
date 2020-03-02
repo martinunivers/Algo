3382cd46a094190b1bcf4112a727256ae59f1570
Martin Bouhenna <bouhenna_martin@yahoo.fr>
2020-03-01 21:09:31 -0500

    correct test function

--- Test_Cases/Test_Algorithms/Test_Sorting/test_bubble.py
+++ Test_Cases/Test_Algorithms/Test_Sorting/test_bubble.py
@@ -3,4 +3,5 @@ import pytest
 #from Algo.Algorithms.Sorting import bubble
 from Algorithms.Sorting import bubble
 
-assert bubble.bubble([4, 3, 1, 0], "ASC") == [0, 1, 3, 4]
+def test_bubble():
+    assert bubble.bubble([4, 3, 1, 0], "ASC") == [0, 1, 3, 4]
