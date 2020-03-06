94ed25c9c38aa86fdfcefb19f0134a015fc8b471
martinunivers <bouhenna_martin@yahoo.fr>
2020-03-06 14:56:01 -0500

    flake8 correction

--- Test_Cases/Test_Algorithms/Test_Sorting/test_bubble.py
+++ Test_Cases/Test_Algorithms/Test_Sorting/test_bubble.py
@@ -6,11 +6,14 @@ from Algorithms.Sorting import bubble
 def test_bubble01():
     assert bubble.bubble([4, 3, 3, 0], "ASC") == [0, 3, 3, 4]
 
+
 def test_bubble02():
     assert bubble.bubble([4], "ASC") == [4]
 
+
 def test_bubble03():
     assert bubble.bubble([], "ASC") == []
+
 
 def test_bubble04():
     assert bubble.bubble([4, 3, 0, 3], "DESC") == [4, 3, 3, 0]
