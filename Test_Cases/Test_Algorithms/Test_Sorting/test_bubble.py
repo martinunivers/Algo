a6174b351461603990c081729ac89a2ebc754bef
martinunivers <bouhenna_martin@yahoo.fr>
2020-03-06 12:15:31 -0500

    Add bubble sort test

--- Test_Cases/Test_Algorithms/Test_Sorting/test_bubble.py
+++ Test_Cases/Test_Algorithms/Test_Sorting/test_bubble.py
@@ -3,5 +3,14 @@ import pytest
 #from Algo.Algorithms.Sorting import bubble
 from Algorithms.Sorting import bubble
 
-def test_bubble():
-    assert bubble.bubble([4, 3, 1, 0], "ASC") == [0, 1, 3, 4]
+def test_bubble01():
+    assert bubble.bubble([4, 3, 3, 0], "ASC") == [0, 3, 3, 4]
+
+def test_bubble02():
+    assert bubble.bubble([4], "ASC") == [4]
+
+def test_bubble03():
+    assert bubble.bubble([], "ASC") == []
+
+def test_bubble04():
+    assert bubble.bubble([4, 3, 0, 3], "DESC") == [4, 3, 3, 0]
